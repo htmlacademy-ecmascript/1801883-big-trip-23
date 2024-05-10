3083fbe97bd9d624c19714fa2058b06421020757
Vladislav <vlad.4sa@gmail.com>
2024-05-10 22:19:49 +0700

    #7. Исправление замечаний (часть 2)

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -1,20 +1,14 @@
 import EventsListView from '../view/events-list-view.js';
 import EventItemView from '../view/event-item-view.js';
-import NewFormView from '../view/form/new-form-view.js';
-import HeaderFormView from '../view/form/header-form-view.js';
-import OffersFormView from '../view/form/offers-form-view.js';
-import OfferItemFormView from '../view/form/offer-item-form-view.js';
-import DestinationFormView from '../view/form/destination-form-view.js';
-import { RenderPosition, render } from '../render.js';
+import FormView from '../view/form-view.js';
+import { render } from '../render.js';
 
 const EVENTS_NUMBER = 3;
+
 
 export default class EventsPresenter {
 
   eventsList = new EventsListView();
-  // newForm = new NewFormView();
-  // eventDetailsContainer = this.newForm.getElement().firstElementChild.firstElementChild;
-  // offersForm = new OffersFormView();
 
   constructor ({eventsContainer}) {
     this.eventsContainerElement = eventsContainer;
@@ -22,11 +16,7 @@ export default class EventsPresenter {
 
   init () {
     render(this.eventsList, this.eventsContainerElement);
-    // render(this.newForm, this.eventsContainer.getElement());
-    // render(new HeaderFormView(), this.newForm.getElement().firstElementChild, RenderPosition.AFTERBEGIN);
-    // render(this.offersForm, this.eventDetailsContainer);
-    // render(new OfferItemFormView, this.offersForm.getElement().children[1]);
-    // render(new DestinationFormView, this.eventDetailsContainer);
+    render(new FormView, this.eventsList.getElement());
 
     for (let i = 0; i < EVENTS_NUMBER; i++) {
       render(new EventItemView(), this.eventsList.getElement());
---
+++ src/view/form-view.js
@@ -0,0 +1,136 @@
+import { createElement } from '../render.js';
+
+const EVENT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
+const IMAGE_LINKS = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+
+
+const createEventTypeItem = (type) => `
+  <div class="event__type-item">
+    <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}" ${type === 'flight' ? 'checked' : ''}>
+    <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${type[0].toUpperCase() + type.slice(1)}</label>
+  </div>
+`;
+
+const createHeader = () => `
+    <header class="event__header">
+      <div class="event__type-wrapper">
+        <label class="event__type  event__type-btn" for="event-type-toggle-1">
+          <span class="visually-hidden">Choose event type</span>
+          <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+        </label>
+        <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+
+        <div class="event__type-list">
+          <fieldset class="event__type-group">
+            <legend class="visually-hidden">Event type</legend>
+            ${EVENT_TYPES.map((value) => createEventTypeItem(value)).join('')}
+          </fieldset>
+        </div>
+      </div>
+
+      <div class="event__field-group  event__field-group--destination">
+        <label class="event__label  event__type-output" for="event-destination-1">
+          Flight
+        </label>
+        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+        <datalist id="destination-list-1">
+          <option value="Amsterdam"></option>
+          <option value="Geneva"></option>
+          <option value="Chamonix"></option>
+        </datalist>
+      </div>
+
+      <div class="event__field-group  event__field-group--time">
+        <label class="visually-hidden" for="event-start-time-1">From</label>
+        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="18/03/19 12:25">
+        &mdash;
+        <label class="visually-hidden" for="event-end-time-1">To</label>
+        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="18/03/19 13:35">
+      </div>
+
+      <div class="event__field-group  event__field-group--price">
+        <label class="event__label" for="event-price-1">
+          <span class="visually-hidden">Price</span>
+          &euro;
+        </label>
+        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="160">
+      </div>
+
+      <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+      <button class="event__reset-btn" type="reset">Delete</button>
+      <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+      </button>
+    </header>
+`;
+
+
+const createOfferItem = () => `
+  <div class="event__offer-selector">
+    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
+    <label class="event__offer-label" for="event-offer-luggage-1">
+      <span class="event__offer-title">Add luggage</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">30</span>
+    </label>
+  </div>
+`;
+
+const createOffers = () => `
+  <section class="event__section  event__section--offers">
+    <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+
+    <div class="event__available-offers">
+    ${createOfferItem()}
+    </div>
+  </section>
+`;
+
+
+const createImageDestination = (imageLink) => `
+  <img class="event__photo" src="img/photos/${imageLink}" alt="Event photo"></img>
+`;
+
+const createDestination = () => `
+  <section class="event__section  event__section--destination">
+    <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+    <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
+
+    <div class="event__photos-container">
+      <div class="event__photos-tape">
+        ${IMAGE_LINKS.map((value) => createImageDestination(value)).join('')}
+      </div>
+    </div>
+  </section>
+`;
+
+
+const createFormTemplate = () => `
+<li class="trip-events__item">
+  <form class="event event--edit" action="#" method="post">
+    ${createHeader()}
+    <section class="event__details">
+      ${createOffers()}
+      ${createDestination()}
+    </section>
+  </form>
+</li>
+`;
+
+export default class FormView {
+  getTemplate() {
+    return createFormTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
