b02fbbfd570a39e34913e3533165a5db45a97e65
Vladislav <vlad.4sa@gmail.com>
2024-06-13 22:39:07 +0700

    15. Посчитал общую стоимость в шапке

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -3,7 +3,7 @@ import AbstractView from '../framework/view/abstract-view';
 
 const createCity = (name, isLastElement) => `${name}${!isLastElement ? ' &mdash; ' : ''}`;
 
-const createTripInfoTemplate = (basePrice, destinations) => `
+const createTripInfoTemplate = (totalPrice, destinations) => `
 <section class="trip-main__trip-info  trip-info">
   <div class="trip-info__main">
     <h1 class="trip-info__title">
@@ -14,25 +14,43 @@ const createTripInfoTemplate = (basePrice, destinations) => `
   </div>
 
   <p class="trip-info__cost">
-    Total: &euro;&nbsp;<span class="trip-info__cost-value">${basePrice}</span>
+    Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
   </p>
 </section>
 `;
 
 export default class TripInfoView extends AbstractView {
-  #totalBasePrice = null;
+  #events = null;
+  #offers = null;
+  #destinations = null;
+  #totalPrice = null;
   #usedIdDestinations = null;
   #usedDestinations = null;
 
-  constructor({events, destinations}) {
+  constructor({events, offers, destinations}) {
     super();
-    this.#totalBasePrice = events.reduce((accumulator, event) => accumulator + event.basePrice, 0);
+    this.#events = events;
+    this.#offers = offers;
+    this.#destinations = destinations;
+
+    this.#totalPrice = this.#events.reduce((accumulator, event) => accumulator + this.#calculateEventCost(event, this.#offers), 0);
 
     this.#usedIdDestinations = new Set(Array.from(events, (event) => event.destination));
     this.#usedDestinations = destinations.filter((destination) => this.#usedIdDestinations.has(destination.id));
   }
 
   get template() {
-    return createTripInfoTemplate(this.#totalBasePrice, this.#usedDestinations);
+    return createTripInfoTemplate(this.#totalPrice, this.#usedDestinations);
+  }
+
+  #calculateEventCost({basePrice, offers, type}, allOffers) {
+    let offersCoast = 0;
+    const allowedOffers = allOffers.find((item) => item.type === type).offers;
+
+    offers.forEach((offer) => {
+      offersCoast += allowedOffers.find((item) => item.id === offer).price;
+    });
+
+    return basePrice + offersCoast;
   }
 }
